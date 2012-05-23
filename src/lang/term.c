b86e75e7eefcee1e86d1818dc872bfe72183a21d
wakamori <wakamori111@gmail.com>
2012-05-24 02:17:08 +0900

    convert yen to back slash

--- src/lang/term.c
+++ src/lang/term.c
@@ -756,7 +756,7 @@ static void Bytes_addQUOTE(CTX ctx, kBytes *ba, kInputStream *in, kline_t *ul, i
 				Bytes_addESC(ctx, ba, in, ul);
 				continue;
 			}
-			else if (ch == 194 && in->io2->buffer[in->io2->top] == 165) {
+			else if (ch == 194 && in->io2->buffer[in->io2->top+1] == 165) {
 				/* Multi Byte Yen */
 				in->io2->top += 1;
 				Bytes_addESC(ctx, ba, in, ul);
@@ -787,7 +787,7 @@ static int Term_addQUOTE(CTX ctx, kTerm *tkB, CWB_t *cwb, kInputStream *in, klin
 		if(ch == '\\' && !isRAW) {
 			Bytes_addESC(ctx, cwb->ba, in, ul);
 		}
-		else if (ch == 194 && in->io2->buffer[in->io2->top] == 165 &&
+		else if (ch == 194 && in->io2->buffer[in->io2->top+1] == 165 &&
 				!isRAW) { /* Multi Byte Yen */
 			in->io2->top += 1;
 			Bytes_addESC(ctx, cwb->ba, in, ul);
