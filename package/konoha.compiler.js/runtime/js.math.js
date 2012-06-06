58e1c15deefc0f7631566f6a4bfb5952f23a87c4
wakamori <wakamori111@gmail.com>
2012-06-06 14:31:22 +0900

    add math methods

--- package/konoha.compiler.js/runtime/js.math.js
+++ package/konoha.compiler.js/runtime/js.math.js
@@ -27,3 +27,30 @@ js.math.Math.fabs = function(f) {
 js.math.Math.random = function() {
     return Math.random();
 }
+js.math.Math.pow = function(a, b) {
+    return Math.pow(a, b);
+}
+js.math.Math.tan = function(f) {
+    return Math.tan(f);
+}
+js.math.Math.toDegrees = function(f) {
+	return f * (Math.PI / 180);
+}
+js.math.Math.toRadians = function(f) {
+	return f * (180 / Math.PI);
+}
+js.math.Math.exp = function(f) {
+	return Math.exp(f);
+}
+js.math.Math.log = function(f) {
+	return Math.log(f);
+}
+js.math.Math.log10 = function(f) {
+	return Math.LOG10E * Math.log(f);
+}
+js.math.Math.getE = function() {
+	return Math.E;
+}
+js.math.Math.getPI = function() {
+	return Math.PI;
+}
